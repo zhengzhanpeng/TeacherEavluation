9e3620c8b460b3219f35f8ee703accc93246a859
zhengzhanpeng <714586750@qq.com>
2017-06-07 19:51:52 +0800

    新增导入学生评教pojo

---
+++ src/com/hdxy/pojo/ScoreInput.java
@@ -0,0 +1,19 @@
+package com.hdxy.pojo;
+
+public class ScoreInput {
+	private String jobNumber;
+	private String score;
+	public String getJobNumber() {
+		return jobNumber;
+	}
+	public void setJobNumber(String jobNumber) {
+		this.jobNumber = jobNumber;
+	}
+	public String getScore() {
+		return score;
+	}
+	public void setScore(String score) {
+		this.score = score;
+	}
+	
+}
